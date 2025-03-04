ad619978e79fe7711ea07b83235ae3d510d202b5
icyatwa <icyatwandoba@gmail.com>
2025-03-04 03:20:18 +0200

    pitch

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const corsOptions = {
       callback(null, false);
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   credentials: true,
   allowedHeaders: [
     'Content-Type', 
